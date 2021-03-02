28ef7d15da7f7f3105df789fbc4427babf662d91
Wolfi <wolfi@disroot.org>
2021-03-02 23:29:54 +0100

    improve panning background

--- scenery/background.v
+++ scenery/background.v
@@ -5,16 +5,21 @@ import lyra
 
 const (
 	path = "resources/scenery/desert/"
-	files = 6
 	ext = ".png"
 )
 
 pub struct Background {
 	mut: items []C.Texture2D
+	scx f32
+	num int
 }
 
 pub fn (mut self Background) load() {
-	for i in 1..6 {
+	// how much eye.cx should affect the background
+	self.scx = .5
+	self.num = 6
+	for i in 1..self.num + 1 {
+		println(i)
 		self.items << vraylib.load_texture(path + i.str() + ext)
 	}
 }
@@ -25,8 +30,8 @@ pub fn (mut self Background) update() {
 
 [live]
 pub fn (self &Background) draw(eye &lyra.Eye) {
-	for item in self.items {
-		vraylib.draw_texture_ex(item, C.Vector2{lyra.start_x - 10, 0}, 0, 1, vraylib.white)
+	for i, item in self.items {
+		vraylib.draw_texture_ex(item, C.Vector2{lyra.start_x + eye.cx * self.scx * (i + 1) / self.num, 0}, 0, 1, vraylib.white)
 	}
 }
 
--- stages/desert.v
+++ stages/desert.v
@@ -2,7 +2,7 @@ module stages
 
 fn desert_props() []Scene {
 	mut one := Scene{}
-	one.add_width = 2200
+	one.add_width = 4200
 	one.color_scheme = [[200, 220, 115, 140, 75, 80],
 		[220, 230, 105, 122, 54, 80],
 	]
